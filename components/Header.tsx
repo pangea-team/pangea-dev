4b5ba4c180accb4ccda6c7a2b65c7642b846ba5a
sunkite <sunkite@sunkiteui-MacBookAir.local>
2026-05-08 16:38:32 +0900

    feat: logo 이미지 webp로 수정 (#23)

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import Icon from '@/components/Icon';
+import logo from '@/public/images/logo.webp';
 
 export default function Header() {
   return (
     <header className="flex h-15 shrink-0 items-center justify-between px-page-x py-2">
       <Link href="/" className="flex items-center gap-5">
-        <Image src="/icons/logo.png" alt="" width={60} height={60} className="shrink-0" priority />
+        <Image src={logo} alt="" width={60} height={60} className="shrink-0" priority />
         <span className="whitespace-nowrap uppercase leading-9 tracking-logo text-cormorant-logo">
           PANGEA
         </span>
